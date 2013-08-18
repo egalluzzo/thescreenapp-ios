5520754827b39327e1a98bb71d3fdd85c72b5dd8
Hitanshu Pande <hitanshu.pande@centricconsulting.com>
2013-08-18 05:48:28 -0400

    Implemented Interview table view - skeleton

---
+++ Screen/Controller/InterviewDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  InterviewDetailViewController.h
+//  Screen
+//
+//  Created by Hitanshu Pande on 8/18/13.
+//  Copyright (c) 2013 Eric Galluzzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InterviewDetailViewController : UITableViewController
+
+@end
---
+++ Screen/Controller/InterviewTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  InterviewTableViewController.h
+//  Screen
+//
+//  Created by Hitanshu Pande on 8/18/13.
+//  Copyright (c) 2013 Eric Galluzzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InterviewTableViewController : UITableViewController
+
+@end
